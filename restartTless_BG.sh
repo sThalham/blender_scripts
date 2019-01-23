283401da2d3fdb252147281d20515913d277a216
Stefan Thalhammer <stefan.thalhammer136@gmail.com>
2019-01-23 09:55:32 +0100

    initial commit

---
+++ restartTless_BG.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
+blender createRealisticScene_wBG_Tless.blend --background --python generatorRealistic_wBG_Tless.py
